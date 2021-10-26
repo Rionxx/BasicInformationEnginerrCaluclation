ec405a1ecade042e239c21bf2f2500bccadf9528
Rion <rionfx.42@gmail.com>
2021-10-26 16:58:59 +0900

    Created Project

---
+++ BasicInformationEngineerCaluclation/BasicInformationEngineerCaluclationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicInformationEngineerCaluclationApp.swift
+//  BasicInformationEngineerCaluclation
+//
+//  Created by FX on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BasicInformationEngineerCaluclationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
